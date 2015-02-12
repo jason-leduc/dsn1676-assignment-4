78dcb57379c4745e08fadd93d0f5aef931ecbf5a
jason-leduc <jasonleduc@Jasons-MacBook-Pro.local>
2015-02-12 16:17:03 -0500

    added dl, dt, and dd elements

--- js/main.js
+++ js/main.js
@@ -24,5 +24,32 @@ var redCurrant = {
   carbohydrates: '13.8 g',
   protein: '1.4 g',
 };
+
+var fruit = [blueberries, grapes, redCurrant];
+
+var checkShrub = function (check) {
+  if(check){
+    return 'Yes';
+	} else {
+      return 'No';
+	}
+}
+
+var writeFruit = function (fruit) {
+	fruit.forEach(function (item) {
+		document.write('<div class="unit unit-s-1 unit-m-1-3 island berry">');
+		document.write('<h2 class="heading">' + item.name + '</h2>');
+		document.write('<dl class="list">');
+		document.write('<dt>Species:</dt><dd>' + item.species + '</dd>');
+		document.write('<dt>Grows on shrub:</dt><dd>' + checkShrub(item.growsOnShrub) + '</dd>');
+		document.write('<dt>Energy:</dt><dd>' + item.energy + '</dd>');
+		document.write('<dt>Carbohydrates:</dt><dd>' + item.carbs + '</dd>');
+		document.write('<dt>Protein:</dt><dd>' + item.protein + '</dd>');
+		document.write('</dl>');
+		document.write('</div>');
+	});
+};
+
+writeFruit(fruit);
   
   
