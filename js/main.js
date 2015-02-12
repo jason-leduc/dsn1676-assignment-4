333f1c1db349228e8108fff6ec6fa93070e2ba71
jason-leduc <jasonleduc@Jasons-MacBook-Pro.local>
2015-02-12 15:48:19 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//alert('Hello!');
+
+//String: just some text always in quotes
+var firstName = 'Jason';
+console.log(firstName);
+
+//Numbers: can be mathified, not in quotes
+var age = 21; 
+
+//Buolean: true or false
+var isHuman = true;
+
+//Array: a box that contains many things
+var dinos = ['Iguanodon', 'Pteranodon', 'T-Rex'];
+console.log(dinos[1]);
+
+//Object: a variable that contains another variable
+var trex = {
+  fullName: 'Tyrannosaurus Rex',
+  age: 60000000,
+  isMeatEater: true
+};
+console.log(trex.fullName);
