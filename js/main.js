a2893bff0a69683577ebf4e116ffb2731beb946c
jason-leduc <jasonleduc@Jasons-MacBook-Pro.local>
2015-02-12 15:54:15 -0500

    added variables for appropriate berries

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-//alert('Hello!');
-
-//String: just some text always in quotes
-var firstName = 'Jason';
-console.log(firstName);
-
-//Numbers: can be mathified, not in quotes
-var age = 21; 
-
-//Buolean: true or false
-var isHuman = true;
-
-//Array: a box that contains many things
-var dinos = ['Iguanodon', 'Pteranodon', 'T-Rex'];
-console.log(dinos[1]);
-
-//Object: a variable that contains another variable
-var trex = {
-  fullName: 'Tyrannosaurus Rex',
-  age: 60000000,
-  isMeatEater: true
+var blueberries = {
+  name: 'blueberries',
+  species: 'Vaccinium corybosum',
+  growsOnShrub: true,
+  energy: '240 kJ',
+  carbohydrates: '14.49 g',
+  protein: '0.74 g',
 };
-console.log(trex.fullName);
+
+var grapes = {
+  name: 'grapes',
+  species: 'vitis vinifera',
+  growsOnShrub: false,
+  energy: '288 kJ',
+  carbohydrates: '18.1 g',
+  protein: '0.72 g',
+};
+
+var redCurrant = {
+  name: 'red currant',
+  species: 'Ribes rubrum',
+  growsOnShrub: true,
+  energy: '234 kJ',
+  carbohydrates: '13.8 g',
+  protein: '1.4 g',
+};
+  
+  
